6f023a5698bb825a8fb63381e7729937027e60b4
averted <vitiuk.alex@gmail.com>
2015-10-06 21:00:43 -0400

    fixing typo

--- server/src/server.js
+++ server/src/server.js
@@ -3,9 +3,7 @@ import Server from 'socket.io';
 export default function startServer(store) {
   const io = new Server().attach(8090);
 
-  store.subscribe(
-    () => io.emit('state', store.getState());
-  );
+  store.subscribe( () => io.emit('state', store.getState()));
 
   io.on('connection', (socket) => {
     socket.emit('state', store.getState());
