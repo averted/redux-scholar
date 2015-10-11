77352e61e49bcc5f8d976c4881ffeffd208bd745
averted <vitiuk.alex@gmail.com>
2015-10-11 10:50:44 -0400

    changing webpack babel transform to allow decorators (stage 1)

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'react-hot!babel',
+      loader: 'react-hot!babel?stage=1',
     }]
   },
   resolve: {
