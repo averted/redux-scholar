8b7f4e9c8b43fad9f94c5904664531f28e564d24
averted <vitiuk.alex@gmail.com>
2015-10-11 15:54:41 -0400

    adding middleware

---
+++ client/src/remote_action_middleware.js
@@ -0,0 +1,9 @@
+export default socket => store => next => action => {
+  console.log('in middleware', action);
+
+  if (action.meta && action.meta.remote) {
+    socket.emit('action', action);
+  }
+
+  return next(action);
+}
